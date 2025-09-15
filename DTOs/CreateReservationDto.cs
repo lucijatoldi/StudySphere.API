5dad80fdc3d9f2d12838ace7e8b0fad2a86f992a
lucijatoldi <toldi.lucija@gmail.com>
2025-09-15 22:30:16 +0200

    docs: Add example values to CreateReservationDto for Swagger UI

--- DTOs/CreateReservationDto.cs
+++ DTOs/CreateReservationDto.cs
@@ -1,10 +1,16 @@
-﻿namespace StudySphere.API.DTOs
+﻿using System.ComponentModel;
+
+namespace StudySphere.API.DTOs
 {
     public class CreateReservationDto
     {
+        [DefaultValue("2025-11-25T10:00:00")]
         public DateTime StartTime { get; set; }
+        [DefaultValue("2025-11-25T11:00:00")]
         public DateTime EndTime { get; set; }
+        [DefaultValue(1)]
         public int StudyRoomId { get; set; }
+        [DefaultValue(1)]
         public int UserId { get; set; }
     }
 }
