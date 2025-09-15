aa04a1d918ac042ae9f8c4193ddb8e408395deb0
lucijatoldi <toldi.lucija@gmail.com>
2025-09-15 22:15:39 +0200

    feat: Seed database with initial study rooms and users

--- Data/ApiDbContext.cs
+++ Data/ApiDbContext.cs
@@ -13,5 +13,21 @@ namespace StudySphere.API.Data
         public DbSet<StudyRoom> StudyRooms { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Reservation> Reservations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<StudyRoom>().HasData(
+                new StudyRoom { Id = 1, Name = "Soba za tihi rad", Capacity = 4, HasWhiteboard = true, HasProjector = false },
+                new StudyRoom { Id = 2, Name = "Konferencijska soba A", Capacity = 12, HasWhiteboard = true, HasProjector = true },
+                new StudyRoom { Id = 3, Name = "Soba za brainstorming", Capacity = 8, HasWhiteboard = true, HasProjector = false }
+            );
+
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Name = "Ana Anić", Email = "ana.anic@primjer.com" },
+                new User { Id = 2, Name = "Marko Marić", Email = "marko.maric@primjer.com" }
+            );
+        }
     }
 }
---
+++ Migrations/20250915201108_SeedInitialData.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StudySphere.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "StudyRooms",
+                columns: new[] { "Id", "Capacity", "HasProjector", "HasWhiteboard", "Name" },
+                values: new object[,]
+                {
+                    { 1, 4, false, true, "Soba za tihi rad" },
+                    { 2, 12, true, true, "Konferencijska soba A" },
+                    { 3, 8, false, true, "Soba za brainstorming" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Name" },
+                values: new object[,]
+                {
+                    { 1, "ana.anic@primjer.com", "Ana Anić" },
+                    { 2, "marko.maric@primjer.com", "Marko Marić" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "StudyRooms",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "StudyRooms",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "StudyRooms",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
